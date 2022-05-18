8459b8a6cb5142ee57f7407125416466d76497de
Antara <antarabanerjee21@yahoo.com>
2022-05-18 16:10:38 +0530

    Security group is created

---
+++ SECURITY GRP/securitygrp.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "secru_grp" {
+  name        = "secru_grp"
+  description = "Aws security group"
+  vpc_id      = "vpc-028d820b8e41ff307"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "secru_grp"
+  }
+}
